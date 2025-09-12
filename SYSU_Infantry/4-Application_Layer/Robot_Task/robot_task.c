d927f43c00afec9e18c026eb06eaccb0156e73b6
circLΣMoon <1140527828@qq.com>
2025-09-12 10:05:06 +0800

    暂时在CMakelist中注释掉施工中的【电机库文件】注释掉主任务中的各个task

--- SYSU_Infantry/4-Application_Layer/Robot_Task/robot_task.c
+++ SYSU_Infantry/4-Application_Layer/Robot_Task/robot_task.c
@@ -17,24 +17,24 @@ osThreadId others_task_handle; //处理其他任务，比如与视觉通信，�
 void Robot_task_init(void)
 {
     //osThreadDef中的形参分别为任务名，任务函数入口，任务优先级，保留参数，栈大小
-    osThreadDef(ins_task, Ins_task, osPriorityAboveNormal, 0, 1024);
-    ins_task_handle = osThreadCreate(osThread(ins_task), NULL); // 由于是阻塞读取传感器,为姿态解算设置较高优先级,确保以1khz的频率执行
-
-    osThreadDef(chassis_task, Chassis_task, osPriorityNormal, 0, 256);
-    chassis_task_handle = osThreadCreate(osThread(chassis_task), NULL);
-
-    osThreadDef(gimbal_task, Gimbal_task, osPriorityNormal, 0, 128);
-    gimbal_task_handle = osThreadCreate(osThread(gimbal_task), NULL);
-
-    osThreadDef(shoot_task, Shoot_task, osPriorityNormal, 0, 1024);
-    shoot_task_handle = osThreadCreate(osThread(shoot_task), NULL);
-
-    osThreadDef(referee_task, Referee_task, osPriorityNormal, 0, 1024);
-    referee_task_handle = osThreadCreate(osThread(referee_task), NULL);
-
-    osThreadDef(rc_task, Rc_task, osPriorityNormal, 0, 1024);
-    rc_task_handle = osThreadCreate(osThread(rc_task), NULL);
-
-    osThreadDef(others_task, Others_task, osPriorityNormal, 0, 1024);
-    others_task_handle = osThreadCreate(osThread(others_task), NULL);
+    // osThreadDef(ins_task, Ins_task, osPriorityAboveNormal, 0, 1024);
+    // ins_task_handle = osThreadCreate(osThread(ins_task), NULL); // 由于是阻塞读取传感器,为姿态解算设置较高优先级,确保以1khz的频率执行
+    //
+    // osThreadDef(chassis_task, Chassis_task, osPriorityNormal, 0, 256);
+    // chassis_task_handle = osThreadCreate(osThread(chassis_task), NULL);
+    //
+    // osThreadDef(gimbal_task, Gimbal_task, osPriorityNormal, 0, 128);
+    // gimbal_task_handle = osThreadCreate(osThread(gimbal_task), NULL);
+    //
+    // osThreadDef(shoot_task, Shoot_task, osPriorityNormal, 0, 1024);
+    // shoot_task_handle = osThreadCreate(osThread(shoot_task), NULL);
+    //
+    // osThreadDef(referee_task, Referee_task, osPriorityNormal, 0, 1024);
+    // referee_task_handle = osThreadCreate(osThread(referee_task), NULL);
+    //
+    // osThreadDef(rc_task, Rc_task, osPriorityNormal, 0, 1024);
+    // rc_task_handle = osThreadCreate(osThread(rc_task), NULL);
+    //
+    // osThreadDef(others_task, Others_task, osPriorityNormal, 0, 1024);
+    // others_task_handle = osThreadCreate(osThread(others_task), NULL);
 }
