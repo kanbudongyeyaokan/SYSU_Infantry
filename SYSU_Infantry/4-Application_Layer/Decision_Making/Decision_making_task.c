c2041fbebb24eab0c7d974f34b6360e7f9d8ffc3
kanbudongyeyaokan <heyk7@mail2.sysu.edu.cn>
2025-09-16 22:34:50 +0800

    决策任务部分内容以及控制模式说明

--- SYSU_Infantry/4-Application_Layer/Decision_Making/Decision_making_task.c
+++
@@ -1,7 +0,0 @@
-//任务头文件
-#include "Decision_Making_task.h"
-
-//API调用
-
-
-
--- SYSU_Infantry/4-Application_Layer/Decision_Making_Task/Decision_making_task.h
+++ SYSU_Infantry/4-Application_Layer/Decision_Making_Task/Decision_making_task.h
@@ -1,15 +1,12 @@
 #ifndef _DECISION_MAKING_TASK_H
 #define _DECISION_MAKING_TASK_H
-/**
- * @brief 机器人决策任务初始化
- *
- */
-void Decision_making_init();
+
 
 /**
- * @brief 机器人决策任务,500Hz频率运行
+ * @brief 机器人决策任务,频率500HZ
  *
  */
 void Decision_making_task();
+
 
 #endif //_DECISION_MAKING_TASK_H
