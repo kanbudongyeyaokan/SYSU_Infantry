d2ce953a20e7ad4c646f11eed635ce5e69d3d9cc
kanbudongyeyaokan <heyk7@mail2.sysu.edu.cn>
2025-09-17 15:07:42 +0800

    1

--- SYSU_Infantry/3-Function_Module_Layer/Decision_making/decision_making.h
+++ SYSU_Infantry/3-Function_Module_Layer/Decision_making/decision_making.h
@@ -9,6 +9,13 @@
 #define RC_CTRL 0
 #define KEYBOARD_CTRL 1
 
+/******************决策任务发送给各个模块的信息**********************/
+//底盘
+typedef struct
+{
+ 
+}Chassis_cmd;
+
 
 /**
  * @brief 根据遥控器左边开关决定机器人是键鼠控制还是遥控器控制,并且调用对应的控制函数
