51a2f7b092132e30c8da089f997a242afec07d0c
circLΣMoon <1140527828@qq.com>
2025-09-20 14:30:38 +0800

    撤销“添加云台”

--- SYSU_Infantry/3-Function_Module_Layer/Gimbal/gimbal.md
+++
@@ -1,3 +0,0 @@
-
-- 初始化函数的other_angle_feedback_ptr这几个other feedback需补充(static attitude_t *gimbal_imu_data)
-- pid初始化按需修改
--- SYSU_Infantry/4-Application_Layer/Execution/Gimbal_Task/Gimbal_task.h
+++ SYSU_Infantry/4-Application_Layer/Execution/Gimbal_Task/Gimbal_task.h
@@ -5,5 +5,4 @@
 #ifndef SYSU_INFANTRY_GIMBAL_TASK_H
 #define SYSU_INFANTRY_GIMBAL_TASK_H
 
-void Gimbal_control_task(void const *argument);
 #endif //SYSU_INFANTRY_GIMBAL_TASK_H
