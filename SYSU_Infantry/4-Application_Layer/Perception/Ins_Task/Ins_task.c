3186b607bda427ae14eb6c0c4c175b471bc5b206
heyaokun <11394380+heyaokun@user.noreply.gitee.com>
2026-02-09 13:21:21 +0800

    一直LK库进来

--- SYSU_Infantry/4-Application_Layer/Perception/Ins_Task/Ins_task.c
+++ SYSU_Infantry/4-Application_Layer/Perception/Ins_Task/Ins_task.c
@@ -27,7 +27,7 @@ void Ins_task(void const *argument)
     {
         Ins_update();
         // 如果需要数据，直接 Ins_get_data()
-        Uart_printf(test_uart,"yaw:%.2f,pitch:%.2f,roll:%.2f,state:%d\r\n",data->euler.yaw,data->euler.pitch,data->euler.roll,data->state);
+        // Uart_printf(test_uart,"yaw:%.2f,pitch:%.2f,roll:%.2f,state:%d\r\n",data->euler.yaw,data->euler.pitch,data->euler.roll,data->state);
         // 打印 欧拉角 + Z轴加速度 + 采样时间dt
         // Uart_printf(test_uart, "Yaw:%.2f, AccZ:%.2f, dt:%.4f\r\n", 
         //             data->euler.yaw, 
