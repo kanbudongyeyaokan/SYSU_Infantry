e3580639e1606f291f2744d4abf40f7a1360ebb1
heyaokun <11394380+heyaokun@user.noreply.gitee.com>
2026-03-08 16:02:33 +0800

    更新

--- SYSU_Infantry/4-Application_Layer/Perception/Ins_Task/Ins_task.c
+++ SYSU_Infantry/4-Application_Layer/Perception/Ins_Task/Ins_task.c
@@ -17,9 +17,9 @@ void Ins_task(void const *argument)
     extern I2C_HandleTypeDef hi2c2;
     extern SPI_HandleTypeDef hspi2;
     // 目前兼容三个IMU驱动：BMI088、HWT101、HWT606
-    const Ins_driver_interface_t *driver = BMI088_Get_Driver();
+    // const Ins_driver_interface_t *driver = BMI088_Get_Driver();
     // const Ins_driver_interface_t *driver = HWT101_IIC_Get_Driver(&hi2c2);
-    // const Ins_driver_interface_t *driver = HWT606_IIC_Get_Driver(&hi2c2); // 获取 HWT606 IIC 驱动接口
+    const Ins_driver_interface_t *driver = HWT606_IIC_Get_Driver(&hi2c2); // 获取 HWT606 IIC 驱动接口
     // const Ins_data_t *data;
     // 初始化 INS 层 
     Ins_init(driver);
@@ -29,12 +29,12 @@ void Ins_task(void const *argument)
         Ins_update();
 
         // 如果需要数据，直接 Ins_get_data()
-        data = Ins_get_data();
-        Uart_printf(test_uart, "Yaw:%.2f, Pitch:%.2f, Temp:%.2f, State:%d\r\n", 
-                    data->euler.yaw, 
-                    data->euler.pitch, 
-                    data->temp, 
-                    data->state);
+        // data = Ins_get_data();
+        // Uart_printf(test_uart, "Yaw:%.2f, Pitch:%.2f, Temp:%.2f, State:%d\r\n", 
+        //             data->euler.yaw, 
+        //             data->euler.pitch, 
+        //             data->temp, 
+        //             data->state);
         //  VOFA_Send(test_uart,
         //       data->euler.yaw,
         //       data->euler.pitch,
