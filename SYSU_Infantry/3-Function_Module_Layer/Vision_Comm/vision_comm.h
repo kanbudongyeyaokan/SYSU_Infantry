d8b676e7956f982cd6fc27521e4b6b48e8a3f1c3
walker2398 <13916981947@163.com>
2026-03-24 19:53:08 +0800

    修正编译错误

--- SYSU_Infantry/3-Function_Module_Layer/Vision_Comm/vision_comm.h
+++ SYSU_Infantry/3-Function_Module_Layer/Vision_Comm/vision_comm.h
@@ -107,5 +107,6 @@ void Vision_Comm_Init(void);
 void Vision_Comm_Parse_Task(void);
 void Vision_Send_Pose(uint32_t time_us, float pitch, float yaw, float pitch_v, float yaw_v, uint16_t current_hp, uint16_t max_hp);
 bool Is_Vision_Online(void);
+const Vision_Ctrl_Data_t* Get_Vision_Ctrl_Data(void);
 
 #endif // __VISION_COMM_H
